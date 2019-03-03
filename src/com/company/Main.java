dce147be92fe1a591f89e14b7fb5eb073a5f86f4
Daria Zubkova <dariaz.2410@gmail.com>
2019-03-03 10:13:23 +0200

    commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,87 @@
+package com.company;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    private static final int BOARD_WIDTH = 500;
+    private static final int BOARD_HEIGHT = 500;
+
+    private int x = 10;
+    private int y = 10;
+    private GraphicsContext gc;
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Point");
+        Canvas canvas = new Canvas();
+        canvas.setWidth(BOARD_WIDTH);
+        canvas.setHeight(BOARD_HEIGHT);
+        BorderPane group = new BorderPane(canvas);
+        Scene scene = new Scene(group);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        gc = canvas.getGraphicsContext2D();
+        draw();
+
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                if (event.getCode() == KeyCode.LEFT) {
+                    System.out.println("LEFT");
+                    if (x > 0) {
+                        x = x - 5;
+                    }
+                }
+                if (event.getCode() == KeyCode.RIGHT) {
+                    System.out.println("RIGHT");
+                    if (x < gc.getCanvas().getWidth() - 30) {
+                        x = x + 5;
+                    }
+                }
+                if (event.getCode() == KeyCode.DOWN) {
+                    System.out.println("DOWN");
+                    if (y < 470) {
+                        y = y + 5;
+                    }
+                }
+                if (event.getCode() == KeyCode.UP) {
+                    if (y > 0) {
+                        System.out.println("UP");
+                        y = y - 5;
+                    }
+                }
+                clear();
+                draw();
+            }
+        });
+    }
+
+    private void clear() {
+        gc.clearRect(0, 0, gc.getCanvas().getWidth(), gc.getCanvas().getHeight());
+    }
+
+    private void draw() {
+        gc.setLineWidth(2);
+        gc.setStroke(Color.BLACK);
+        gc.strokeOval(x, y, 30, 30);
+        gc.setFill(Color.GREEN);
+        gc.fillRect(x+5, y+5, 20,20);
+    }
+
+}
